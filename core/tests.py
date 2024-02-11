042028c704866bacabe1dc321fce39233c64c144
WesleyVitor <wesleydemorais@outlook.com.br>
2024-02-11 14:45:33 -0300

    test: using permission with token

--- core/tests.py
+++ core/tests.py
@@ -1,8 +1,16 @@
-from rest_framework.test import APITestCase
 from django.urls import reverse
+
+from rest_framework.test import APITestCase
+from model_bakery import baker
+
+from core.services import JWTAuthenticationService
 
 class HomeTest(APITestCase):
     def test_response_hello_world(self):
-        response = self.client.get(reverse('home'))
+        user = baker.make("User", email="test@gmail.com", password="1234-abc", username="teste")
+
+        token = JWTAuthenticationService.generate_access_token(user)['token']
+        
+        response = self.client.get(reverse('home'),  format='json', HTTP_AUTHORIZATION=f'Bearer {token}')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data, {'message': 'Hello World!'})
